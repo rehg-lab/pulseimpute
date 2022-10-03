ecdf9343a8956f178c24e99992796d764b6f89d8
Max Xu <maxxu05@gmail.com>
2022-10-03 13:10:05 -0400

    reverted back get_data

--- get_data.sh
+++ get_data.sh
@@ -1,5 +1,5 @@
 wget -P data/ https://www.dropbox.com/sh/6bygnzzx5t970yx/AAAHsVu9WeVXdQ_c1uBy_WkAa?dl=0
 
-unzip data/AAAHsVu9WeVXdQ_c1uBy_WkAa?dl=0.1 -x -d data/
+unzip data/AAAHsVu9WeVXdQ_c1uBy_WkAa?dl=0 -x -d data/
 
 rm data/AAAHsVu9WeVXdQ_c1uBy_WkAa?dl=0

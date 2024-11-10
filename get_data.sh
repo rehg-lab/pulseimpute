5d517829d2a423d13e99b092006fc30a3b6fc188
Xu <maxu@cc-login5.campuscluster.illinois.edu>
2024-11-09 20:08:40 -0600

    moved demo

--- get_data.sh
+++
@@ -1,5 +0,0 @@
-wget -P data/ https://zenodo.org/records/7129965/files/pulseimpute_data.zip
-
-unzip data/pulseimpute_data.zip -x -d data/
-
-rm data/pulseimpute_data.zip
--- models/BRITSModel_Architecture/src/models/__init__.py
+++
@@ -1,1 +0,0 @@
-# import .rits_i, .brits_i, .rits, .brits, .gru_d, .m_rnn
